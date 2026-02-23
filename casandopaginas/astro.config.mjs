438b35770713503e9d9d5c7005a77eee88e066e0
andrewblockernst <andrewblockernst@gmail.com>
2026-02-23 03:05:29 -0300

    Fotos agregadas.

--- casandopaginas/astro.config.mjs
+++ casandopaginas/astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: vercel(),
   integrations: [tailwind()],
 });
